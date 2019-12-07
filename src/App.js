2c0fcef84091a0c3478aa2a306453ed4c84382e0
Vanz Investor <vanzinvestor@gmail.com>
2019-12-07 20:34:13 +0700

    Navbar Component Logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.scss';
+import Navbar from './components/Navbar/Navbar';
 
 class App extends Component {
   render() {
-    return <h1>Our current app</h1>;
+    return (
+      <Fragment>
+        <Navbar />
+      </Fragment>
+    );
   }
 }
 
