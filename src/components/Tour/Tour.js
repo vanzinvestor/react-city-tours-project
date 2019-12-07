16f97dd0490fac50859d355390b182163d9e768a
Vanz Investor <vanzinvestor@gmail.com>
2019-12-07 21:13:47 +0700

    TourList Component

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './tour.scss';
 
 class Tour extends Component {
   render() {
--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import Tour from '../Tour';
+import './tourlist.scss';
+import { tourData } from '../../tourData';
 
 class TourList extends Component {
+  state = {
+    tours: tourData,
+  };
+
+  removeTour = id => {
+    console.log(id);
+  };
+
   render() {
+    // console.log(this.state.tours);
+    const { tours } = this.state;
+
     return (
-      <div>
-        Hello from Tourlist
-        <Tour />
-      </div>
+      <section className="tourlist">
+        {tours.map(tour => (
+          <Tour key={tour.id} tour={tour} removeTour={this.removeTour}></Tour>
+        ))}
+      </section>
     );
   }
 }
