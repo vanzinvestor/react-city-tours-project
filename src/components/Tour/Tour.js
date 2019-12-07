5fad5fe41b8b8d9cc956bfcfdf06ffc93d35c670
Vanz Investor <vanzinvestor@gmail.com>
2019-12-07 21:26:33 +0700

    Tour Component Logic

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -3,7 +3,30 @@ import './tour.scss';
 
 class Tour extends Component {
   render() {
-    return <h1>Hello Tour</h1>;
+    const { id, city, img, name, info } = this.props.tour;
+    const { removeTour } = this.props;
+
+    return (
+      <article className="tour">
+        <div className="img-container">
+          <img src={img} alt="figure of the tour" />
+          <span className="close-btn">
+            <i className="fas fa-window-close"></i>
+          </span>
+        </div>
+        <div className="tour-info">
+          <h3>{city}</h3>
+          <h4>{name}</h4>
+          <h5>
+            info{' '}
+            <span>
+              <i className="fas fa-caret-square-down"></i>
+            </span>
+          </h5>
+          <p>{info}</p>
+        </div>
+      </article>
+    );
   }
 }
 
